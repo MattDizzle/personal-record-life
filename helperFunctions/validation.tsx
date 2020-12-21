f893abbe5b72f24d821215e82ce99bb350773d65
kidus yilma <kidusyilma@gmail.com>
2020-12-21 12:30:52 -0800

    changed signup to class component

--- helperFunctions/validation.tsx
+++ helperFunctions/validation.tsx
@@ -1,21 +1,5 @@
-export const formValidation = (userInput) => {
+export const emailValidation = (userInput) => {
     let regexEmail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-
-    if (!userInput.userName || !userInput.email || !userInput.password || !userInput.rePassword) {
-        return {
-            error: true,
-            errorMessage: 'All Fields Are Required',
-
-        }
-    }
-
-    else if (userInput.password !== userInput.rePassword) {
-        return {
-            error: true,
-            errorMessage: "Password Doesn't Match"
-        }
-    }
-
     if (!userInput.email.match(regexEmail)) {
         return {
             error: true,
@@ -23,15 +7,38 @@ export const formValidation = (userInput) => {
         };
     }
 
-    else {
-        return {
-            error: false,
-            errorMessage: ''
-        }
+    return {
+        error: false,
+        errorMessage: ""
     }
 
 }
 
-export default {
-    formValidation
+export const formValidation = (userInput) => {
+    if (!userInput.userName || !userInput.email || !userInput.password || !userInput.rePassword) {
+        return {
+            error: true,
+            errorMessage: 'All Fields Are Required',
+
+        }
+    }
+    return {
+        error: false,
+        errorMessage: ""
+    }
 }
+
+
+export const passwordValidation = (userInput) => {
+    if (userInput.password !== userInput.rePassword) {
+        return {
+            error: true,
+            errorMessage: "Password Doesn't Match"
+        }
+    }
+    return {
+        error: false,
+        errorMessage: ""
+    }
+}
+
--- screens/Signup.tsx
+++ screens/Signup.tsx
@@ -7,97 +7,88 @@ import bgImage from '../assets/images/yLOOp.jpg'
 import PRlogo from '../assets/images/PRLogo1white.png';
 
 //validation import 
-import { formValidation } from '../helperFunctions/validation'
+import { formValidation, emailValidation, passwordValidation } from '../helperFunctions/validation'
 
-export default function Signup() {
-  const reducer = (state, action) => {
+export default class Signup extends React.Component {
 
-    switch (action.type) {
-      case 'userName':
-        return { ...state, userName: action.payload }
-      case 'email':
-        return { ...state, email: action.payload }
-      case 'password':
-        return { ...state, password: action.payload }
-      case 'rePassword':
-        return { ...state, rePassword: action.payload }
-
-      default:
-        return state
-    }
-
+  state = {
+    userName: '',
+    email: '',
+    password: '',
+    rePassword: '',
+    error: null
 
   }
+  render() {
+    return (
+      <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+        <View style={styles.container}>
+          <Image source={PRlogo} style={styles.logo} />
+          {this.state.error ? <Text style={styles.error}>{this.state.error} </Text> : <Text> </Text>}
 
-  const [state, dispatch] = useReducer(reducer, { userName: null, email: null, password: null, rePassword: null })
-  const [error, setError] = useState(null)
+          <Text style={styles.title}>Create a Username</Text>
+          <TextInput style={styles.input} value={this.state.userName} onChangeText={(input) => this.setState({ userName: `${input}` })} onBlur={() => {
+            let isError = formValidation(this.state)?.error
+            if (isError) {
+              let error = formValidation(this.state).errorMessage
+              this.setState({ error });
+            }
+            else {
+              this.setState({ error: null });
+            }
+
+          }} />
+          <Text style={styles.title}>Enter your email </Text>
+          <TextInput style={styles.input} value={this.state.email} onChangeText={(input) => this.setState({ email: `${input}` })} onBlur={() => {
+            let isError = emailValidation(this.state)?.error
+            if (isError) {
+              let error = emailValidation(this.state).errorMessage
+              this.setState({ error });
+            }
+            else {
+              this.setState({ error: null });
+            }
+
+          }} />
+          <Text style={styles.title}>Password </Text>
+          <TextInput style={styles.input} value={this.state.password} onChangeText={(input) => this.setState({ password: `${input}` })} onBlur={() => {
+            let isError = passwordValidation(this.state)?.error
+            if (isError) {
+              let error = passwordValidation(this.state).errorMessage
+              this.setState({ error });
+            }
+            else {
+              this.setState({ error: null })
+            }
+
+          }} />
+          <Text style={styles.title}>Retype Password </Text>
+          <TextInput style={styles.input} value={this.state.rePassword} onChangeText={(input) => this.setState({ rePassword: `${input}` })} onBlur={() => {
+            let isError = passwordValidation(this.state)?.error
+            if (isError) {
+              let error = passwordValidation(this.state).errorMessage
+              this.setState({ error });
+            }
+            else {
+              this.setState({ error: null })
+            }
+
+          }} />
+          <Button
+            onPress={() => Alert.alert('This button works')}
+            title="Let's set a new Record"
+            color="#006400"
+            accessibilityLabel="Submit new user information"
+            disabled={!!this.state.error}
+
+          />
+        </View>
+      </ImageBackground>
+    );
+  }
 
 
-  return (
-    <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-      <View style={styles.container}>
-        <Image source={PRlogo} style={styles.logo} />
-        {error ? <Text style={styles.error}>{error} </Text> : <Text> </Text>}
 
-        <Text style={styles.title}>Create a Username</Text>
-        <TextInput style={styles.input} value={state.userName} onChangeText={(input) => dispatch({ type: 'userName', payload: `${input}` })} onBlur={() => {
-          let isError = formValidation(state)?.error
-          if (isError) {
-            let error = formValidation(state).errorMessage
-            setError(error);
-          }
-          else {
-            setError(null)
-          }
-
-        }} />
-        <Text style={styles.title}>Enter your email </Text>
-        <TextInput style={styles.input} value={state.email} onChangeText={(input) => dispatch({ type: 'email', payload: `${input}` })} onBlur={() => {
-          let isError = formValidation(state)?.error
-          if (isError) {
-            let error = formValidation(state).errorMessage
-            setError(error);
-          }
-          else {
-            setError(null)
-          }
-
-        }} />
-        <Text style={styles.title}>Password </Text>
-        <TextInput style={styles.input} value={state.password} onChangeText={(input) => dispatch({ type: 'password', payload: `${input}` })} onBlur={() => {
-          let isError = formValidation(state)?.error
-          if (isError) {
-            let error = formValidation(state).errorMessage
-            setError(error);
-          }
-          else {
-            setError(null)
-          }
-
-        }} />
-        <Text style={styles.title}>Retype Password </Text>
-        <TextInput style={styles.input} value={state.rePassword} onChangeText={(input) => dispatch({ type: 'rePassword', payload: `${input}` })} onBlur={() => {
-          let isError = formValidation(state)?.error
-          if (isError) {
-            let error = formValidation(state).errorMessage
-            setError(error);
-          }
-          else {
-            setError(null)
-          }
-
-        }} />
-        <Button
-          onPress={() => Alert.alert('This button works')}
-          title="Let's set a new Record"
-          color="#006400"
-          accessibilityLabel="Submit new user information"
-          disabled={!!error}
-
-        />
-      </View>
-    </ImageBackground>
-  );
 }
 
 const styles = StyleSheet.create({
